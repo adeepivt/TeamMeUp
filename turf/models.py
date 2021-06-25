630e70996c438957341259e6253e219584facd17
adeepivt <adeepivt@gmail.com>
2021-06-25 20:15:13 +0530

    added turfs

--- turf/models.py
+++ turf/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Turf(models.Model):
+    name = models.CharField(max_length=100)
+    place = models.CharField()
+    manager = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey()
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
--- turf/views.py
+++ turf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Turf
 # Create your views here.
+
+def turf_list(request):
+    turfs = Turf.objects.all()
+    content = {
+        'turfs' : turfs
+    }
+    return render(request, 'turf/turf_list.html', content)
